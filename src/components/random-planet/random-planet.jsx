ae54663531a9df4f019581689394fa128d3d9052
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-17 21:27:29 +0300

    1.3 Add methods to the RandomPlanet to recive data from the server and add Spinner.

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -1,32 +1,72 @@
 import React, {PureComponent} from 'react';
 import './random-planet.css';
+import Spinner from '../spinner/spinner';
+
+import ApiService from '../../services/api-service';
 
 export default class RandomPlanet extends PureComponent {
 
+  _apiService = new ApiService();
+
+  state = {
+    planet: {
+      id: 2,
+    },
+    loading: true,
+  };
+
+  componentDidMount() {
+    this._updatePlanet(Math.floor(Math.random() * 20)  + 2);
+  }
+
+
+  _onPlanetLoaded = (planet) => {
+    this.setState({planet, loading: false});
+  };
+
+  _updatePlanet(id) {
+    this._apiService
+      .getPlanet(id)
+      .then(this._onPlanetLoaded);
+  }
+
   render() {
+    const {planet, loading} = this.state;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView {...planet}/> : null;
+
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-              src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
-              alt="random planet"/>
-        <div>
-          <h4>Planet Name</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>123124</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const PlanetView = ({id, name, population, rotationPeriod, diameter}) => {
+  return (
+    <React.Fragment>
+      <img className="planet-image"
+              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+              alt="random planet"/>
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner__ring-wrapper">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
+
